a816567b0628cfa9523eb8b3c30a7560fe19b107
Matteo Corradini <matteo.corradini@tenet.company>
2022-08-09 12:05:30 +0200

    Add suricata rules reload

--- web/apiv2/views_custom.py
+++ web/apiv2/views_custom.py
@@ -6,7 +6,9 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from lib.cuckoo.common.cape_utils import init_yara
+from lib.cuckoo.common.config import Config
 from lib.cuckoo.common.constants import CUCKOO_ROOT
+from lib.cuckoo.common.suricatasc import SuricataSC
 
 YARA_BASE_FOLDER = os.path.join(CUCKOO_ROOT, "data", "yara")
 YARA_CUSTOM_RULE_FOLDERS = [os.path.join(YARA_BASE_FOLDER, "custom"), os.path.join(YARA_BASE_FOLDER, "memory")]
@@ -64,6 +66,14 @@ def clean_up_yara(request):
     return Response({'error': False})
 
 
+def update_suricata_rules():
+    c = Config('processing')
+    suricata_socket_path = c.suricata.get("socket_file")
+    suris = SuricataSC(suricata_socket_path)
+
+    suris.send_command('ruleset-reload-nonblocking')
+
+
 @csrf_exempt
 @api_view(["POST"])
 def upload_snort(request):
@@ -79,6 +89,7 @@ def upload_snort(request):
     with open(full_path, 'w') as f:
         f.write(body)
 
+    update_suricata_rules()
     return Response({'snort': filename, 'error': False})
 
 
@@ -91,6 +102,7 @@ def delete_snort(request, _id):
     except FileNotFoundError:
         return Response({'error_value': 'Snort not found', 'error': True})
 
+    update_suricata_rules()
     return Response({'error': False})
 
 
@@ -105,4 +117,5 @@ def clean_up_snort(request):
             # Rule is not a custom one
             pass
 
+    update_suricata_rules()
     return Response({'error': False})
