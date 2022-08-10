7a4c0c98a407f4dee273efbb50760bfd025fc33f
Matteo Corradini <matteo.corradini@tenet.company>
2022-08-10 14:54:43 +0200

    Add error rules mgmt and refactoring

--- web/apiv2/views_custom.py
+++ web/apiv2/views_custom.py
@@ -1,3 +1,4 @@
+import json
 import os
 import uuid
 
@@ -5,49 +6,63 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from lib.cuckoo.common.cape_utils import init_yara
+from lib.cuckoo.common.cape_utils import init_yara, log
 from lib.cuckoo.common.config import Config
 from lib.cuckoo.common.constants import CUCKOO_ROOT
-from lib.cuckoo.common.suricatasc import SuricataSC
+from lib.cuckoo.common.suricatasc import SuricataSC, SuricataCommandException
 
 YARA_BASE_FOLDER = os.path.join(CUCKOO_ROOT, "data", "yara")
 YARA_CUSTOM_RULE_FOLDERS = [os.path.join(YARA_BASE_FOLDER, "custom"), os.path.join(YARA_BASE_FOLDER, "memory")]
-SNORT_BASE_FOLDER = os.path.join("etc", "suricata", "rules")
+SNORT_BASE_FOLDER = os.path.join("/", "etc", "suricata", "rules")
 
 
 @csrf_exempt
 @api_view(["POST"])
-def upload_yara(request):
+def upload_yara(request, reload_rule: bool = True):
     try:
-        _id = request.data['id']
-        body = request.data['body']
+        _id = request.data["id"]
+        body = request.data["body"]
     except KeyError:
-        return Response({'error': True, 'error_value': 'id or body not set in request'})
+        return Response({"error": True, "error_value": "id or body not set in request"})
 
-    filename = _id + '.yar'
+    filename = _id + ".yar"
 
     for folder in YARA_CUSTOM_RULE_FOLDERS:
         full_path = os.path.join(folder, filename)
 
-        with open(full_path, 'w') as f:
+        with open(full_path, "w") as f:
             f.write(body)
 
-    init_yara()
-    return Response({'yara': filename, 'error': False})
+    if reload_rule:
+        try:
+            init_yara()
+        except Exception as e:
+            for folder in YARA_CUSTOM_RULE_FOLDERS:
+                full_path = os.path.join(folder, filename)
+                os.remove(full_path)
+
+            return {"error": True, "error_value": f"Unable to refresh Yara rule due to {e}"}
+
+    return Response({"yara": filename, "error": False})
 
 
 @csrf_exempt
 @api_view(["DELETE"])
-def delete_yara(request, _id):
+def delete_yara(request, _id, reload_rule: bool = True):
     for folder in YARA_CUSTOM_RULE_FOLDERS:
         full_path = os.path.join(folder, f"{_id}.yar")
         try:
             os.remove(full_path)
         except FileNotFoundError:
-            return Response({'error_value': 'Yara not found', 'error': True})
+            return Response({"error_value": "Yara not found", "error": True})
 
-    init_yara()
-    return Response({'error': False})
+    if reload_rule:
+        try:
+            init_yara()
+        except Exception as e:
+            return {"error": True, "error_value": f"Unable to refresh Yara rule due to {e}"}
+
+    return Response({"error": False})
 
 
 @csrf_exempt
@@ -62,48 +77,72 @@ def clean_up_yara(request):
                 # Rule is not a custom one
                 pass
 
-    init_yara()
-    return Response({'error': False})
+    try:
+        init_yara()
+    except Exception as e:
+        return {"error": True, "error_value": f"Unable to refresh Yara rule due to {e}"}
+
+    return Response({"error": False})
 
 
 def update_suricata_rules():
-    c = Config('processing')
+    c = Config("processing")
     suricata_socket_path = c.suricata.get("socket_file")
-    suris = SuricataSC(suricata_socket_path)
 
-    suris.send_command('ruleset-reload-nonblocking')
+    suris = SuricataSC(suricata_socket_path)
+    suris.connect()
+    return suris.send_command("ruleset-reload-nonblocking")
 
 
 @csrf_exempt
 @api_view(["POST"])
-def upload_snort(request):
+def upload_snort(request, reload_rule: bool = True):
     try:
-        _id = request.data['id']
-        body = request.data['body']
+        _id = request.data["id"]
+        body = request.data["body"]
     except KeyError:
-        return Response({'error': True, 'error_value': 'id or body not set in request'})
+        return Response({"error": True, "error_value": "id or body not set in request"})
 
-    filename = _id + '.rules'
+    filename = _id + ".rules"
     full_path = os.path.join(SNORT_BASE_FOLDER, filename)
 
-    with open(full_path, 'w') as f:
+    with open(full_path, "w") as f:
         f.write(body)
 
-    update_suricata_rules()
-    return Response({'snort': filename, 'error': False})
+    if reload_rule:
+        try:
+            cmd_out = update_suricata_rules()
+
+            if cmd_out["return"].upper() not in ["NOK", "OK"]:
+                raise SuricataCommandException(f"ruleset-reload-nonblocking command return with {cmd_out}")
+
+        except Exception as e:
+            os.remove(full_path)
+            return {"error": True, "error_value": f"Unable to refresh Snort rule due to {e}"}
+
+    return Response({"snort": filename, "error": False})
 
 
 @csrf_exempt
 @api_view(["DELETE"])
-def delete_snort(request, _id):
+def delete_snort(request, _id, reload_rule: bool = True):
     full_path = os.path.join(SNORT_BASE_FOLDER, f"{_id}.rules")
     try:
         os.remove(full_path)
     except FileNotFoundError:
-        return Response({'error_value': 'Snort not found', 'error': True})
+        return Response({"error_value": "Snort not found", "error": True})
 
-    update_suricata_rules()
-    return Response({'error': False})
+    if reload_rule:
+        try:
+            cmd_out = update_suricata_rules()
+
+            if cmd_out["return"].upper() not in ["NOK", "OK"]:
+                raise SuricataCommandException(f"ruleset-reload-nonblocking command return with {cmd_out}")
+
+        except Exception as e:
+            return {"error": True, "error_value": f"Unable to refresh Snort rule due to {e}"}
+
+    return Response({"error": False})
 
 
 @csrf_exempt
@@ -117,5 +156,13 @@ def clean_up_snort(request):
             # Rule is not a custom one
             pass
 
-    update_suricata_rules()
-    return Response({'error': False})
+    try:
+        cmd_out = update_suricata_rules()
+
+        if cmd_out["return"].upper() not in ["NOK", "OK"]:
+            raise SuricataCommandException(f"ruleset-reload-nonblocking command return with {cmd_out}")
+
+    except Exception as e:
+        return {"error": True, "error_value": f"Unable to refresh Snort rule due to {e}"}
+
+    return Response({"error": False})
